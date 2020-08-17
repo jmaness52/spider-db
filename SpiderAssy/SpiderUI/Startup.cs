269a2980dee5c0499dca0d49b8d144ff3b60b26f
jmaness52 <45572832+jmaness52@users.noreply.github.com>
2020-08-16 22:02:45 -0600

    moved identity DB to SQlite

--- SpiderAssy/SpiderUI/Startup.cs
+++ SpiderAssy/SpiderUI/Startup.cs
@@ -33,8 +33,8 @@ namespace SpiderUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(
+                    Configuration.GetConnectionString("IdentitySQLite")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
